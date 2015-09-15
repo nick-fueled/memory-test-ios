c8740554e363cb0e9f48619975dc38f886b00ec5
Nick McConnell <ngmcconnell@yahoo.com>
2015-09-14 21:28:20 -0500

    initial project setup

--- memory-test/ViewController.swift
+++ memory-test/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+//import AppKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    @IBAction func buttonTapped(sender: AnyObject) {
+        var a : ClassA?
+        for i in 0 ..< 1000 {
+            a = ClassA();
+        }
+    }
+    
+    
+    @IBAction func otherButtonTapped(sender: AnyObject) {
+        
+            for _ in 0 ..< 5 {
+                autoreleasepool {
+                    for _ in 0 ..< 1000 {
+                        let image = UIImage(named: "Anything_can_happen")
+                    }
+                }
+            }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func withoutButtonTapped(sender: AnyObject) {
+        
+        for _ in 0 ..< 5 {
+                for _ in 0 ..< 1000 {
+                    let image = UIImage(named: "Anything_can_happen")
+                }
+        }
     }
 
 
 }
 
+
+// Retain cycle
+
+class ClassA {
+    
+    internal var otherClassB : ClassB?
+    
+    init() {
+        otherClassB = ClassB()
+        otherClassB?.setA(self)
+    }
+}
+
+class ClassB {
+    
+    internal var otherClassA : ClassA?
+    
+    func setA(a:ClassA) {
+        otherClassA = a
+    }
+}
